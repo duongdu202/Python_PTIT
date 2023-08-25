ea6985316970a001c56a41d8295d555d666bf4bc
Dương Quang Dự <80323471+duongdu2k2@users.noreply.github.com>
2023-08-25 16:24:30 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.write('Họ và tên: Nguyễn Văn B')
+st.write('Họ và tên: Nguyễn Văn Hưng')
 st.write('Ngày tháng năm sinh: 01/01/2001')
 st.write('Môn học yêu thích: Môn toán')
 st.write('Sở thích: Chơi thể thao, đọc sách')
